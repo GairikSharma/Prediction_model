c88759de75031e74b774676adcef0e31f3c3bd2f
GairikSharma <gairiksharma202@gmail.com>
2023-11-04 21:15:13 +0530

    update

--- client/index.js
+++ client/index.js
@@ -10,25 +10,3 @@ var modalContent = document.getElementById('modal-content')
 modalContent.onclick = function (event) {
   event.stopPropagation()
 }
-// function initMap() {
-//     // Your map initialization code here
-//     const mapElement = document.getElementById("map");
-//     const locationInput = document.getElementById("locationInput").value;
-
-//     // You can use a geocoding service to convert the input into coordinates.
-//     // For this example, we'll use a placeholder data.
-//     const latitude = 37.7749;
-//     const longitude = -122.4194;
-
-//     const map = new google.maps.Map(mapElement, {
-//         center: { lat: latitude, lng: longitude },
-//         zoom: 10, // Adjust the zoom level as needed
-//     });
-
-//     // You can add a zoom-in animation using the setZoom method.
-//     map.setZoom(14);
-// }
-
-// function showLocation() {
-//     initMap();
-// }
